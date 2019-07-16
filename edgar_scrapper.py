b66810e8072185587f4926ee9cd7756eacaf7f8b
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-16 00:34:22 -0400

    update

---
+++ edgar_scrapper.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+  name = 'blogspider'
+  start_urls = ['https://www.sec.gov/cgi-bin/browse-edgar?CIK=0001166559&owner=exclude&action=getcompany&Find=Search']
+
+  def parse(self, response):
+    filings = []
+    for filing in response.css('tr'):
+      filings.append(filing.css('td::text').get())
+    # for next_page in response.css('a.next-posts-link'):
+    #     yield response.follow(next_page, self.parse)
+    print(filings)
+    print(len(filings))
