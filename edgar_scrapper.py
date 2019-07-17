764382c38d6ee5cc6d0ffe202bfa9e48dafb4784
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-16 22:49:15 -0400

    parse table info into delimited txt file

--- edgar_scrapper.py
+++ edgar_scrapper.py
@@ -3,7 +3,8 @@ import scrapy
 class BlogSpider(scrapy.Spider):
   name = 'blogspider'
   cik = '0001166559'
-  start_url = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK=' + cik + '&owner=exclude&action=getcompany&Find=Search'
+  # start_url = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK=' + cik + '&owner=exclude&action=getcompany&Find=Search'
+  start_url = 'https://www.sec.gov/Archives/edgar/data/1166559/000110465919029714/0001104659-19-029714-index.htm'
   start_urls = [start_url]
 
   # def parse(self, response):
@@ -25,13 +26,32 @@ class BlogSpider(scrapy.Spider):
   #   # print(filings)
   #   print(response.url)
 
+  # def parse(self, response):
+  #   filings = []
+  #   for filing in response.css('tr'):
+  #     file = filing.css('td::text').get()
+  #     if file is not None and "13F" in file:
+  #       filings.append(filing.css('td::text').get())
+  #   # for next_page in response.css('a.next-posts-link'):
+  #   #     yield response.follow(next_page, self.parse)
+  #   print(filings)
+  #   print(len(filings))
+
   def parse(self, response):
-    filings = []
-    for filing in response.css('tr'):
-      file = filing.css('td::text').get()
-      if file is not None and "13F" in file:
-        filings.append(filing.css('td::text').get())
-    # for next_page in response.css('a.next-posts-link'):
-    #     yield response.follow(next_page, self.parse)
-    print(filings)
-    print(len(filings))
+    f = open("data.txt","w+")
+    products = response.css('tr')
+    # ignore the table header row
+    for product in products[1:]:
+       item = dict()
+       item['seq'] = product.xpath('td[1]//text()').extract_first()
+       item['description'] = product.xpath('td[2]//text()').extract_first()
+       item['document'] = product.xpath('td[3]//text()').extract_first()
+       item['type'] = product.xpath('td[3]//text()').extract_first()
+       item['size'] = product.xpath('td[3]//text()').extract_first()
+       for k, v in item.items():
+        if v is None:
+          item[k] = ""
+       val_string = ','.join(item.values())
+       f.write(val_string + '\n')
+    # print(filings)
+    # print(len(filings))
