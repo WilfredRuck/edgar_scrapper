433959db249f68c6b23f37f1feb1c8dad7885a8a
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-20 16:27:03 -0400

    merge all page requests

--- edgar_scrapper.py
+++ edgar_scrapper.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 class BlogSpider(scrapy.Spider):
   name = 'blogspider'
   cik = '0001166559'
-  # start_url = 'https://www.sec.gov/cgi-bin/browse-edgar?CIK=' + cik + '&owner=exclude&action=getcompany&Find=Search'
+  base_url = 'https://www.sec.gov'
+  xml_page_url = ''
   # start_url = 'https://www.sec.gov/Archives/edgar/data/1166559/000110465919029714/0001104659-19-029714-index.htm'
   start_url = 'https://www.sec.gov/edgar/searchedgar/companysearch.html'
   start_urls = [start_url]
@@ -17,43 +18,40 @@ class BlogSpider(scrapy.Spider):
         callback=self.parse_filings
     )
 
-  def parse_filings(self, response):
-    # filings = []
-    # for filing in response.css('div#contentDiv'):
-    #   filings.append(filing.css('div.noCompanyMatch::text').get())
-      # if file is not None and "13F" in file:
-      #   filings.append(filing.css('td::text').get())
-    # for next_page in response.css('a.next-posts-link'):
-    #     yield response.follow(next_page, self.parse)
-    # print(filings)
-    print(response.url)
-
-  # def parse(self, response):
+  # def parse_filings(self, response):
   #   filings = []
-  #   for filing in response.css('tr'):
-  #     file = filing.css('td::text').get()
+  #   for filing in response.css('div#contentDiv'):
+  #     filings.append(filing.css('div.noCompanyMatch::text').get())
   #     if file is not None and "13F" in file:
   #       filings.append(filing.css('td::text').get())
-  #   # for next_page in response.css('a.next-posts-link'):
-  #   #     yield response.follow(next_page, self.parse)
+  #   for next_page in response.css('a.next-posts-link'):
+  #       yield response.follow(next_page, self.parse_filings)
   #   print(filings)
-  #   print(len(filings))
+  #   print(response.url)
 
-  # def parse(self, response):
-  #   f = open("data.txt","w+")
-  #   products = response.css('tr')
-  #   # ignore the table header row
-  #   for product in products[1:]:
-  #      item = dict()
-  #      item['seq'] = product.xpath('td[1]//text()').extract_first()
-  #      item['description'] = product.xpath('td[2]//text()').extract_first()
-  #      item['document'] = product.xpath('td[3]//text()').extract_first()
-  #      item['type'] = product.xpath('td[3]//text()').extract_first()
-  #      item['size'] = product.xpath('td[3]//text()').extract_first()
-  #      for k, v in item.items():
-  #       if v is None:
-  #         item[k] = ""
-  #      val_string = ','.join(item.values())
-  #      f.write(val_string + '\n')
-    # print(filings)
-    # print(len(filings))
+  def parse_filings(self, response):
+    for filing in response.css('tr'):
+      file = filing.css('td::text').get()
+      if file is not None and "13F" in file:
+        self.xml_page_url = filing.css('a#documentsbutton::attr(href)').extract()[0]
+        break
+    yield scrapy.Request(self.base_url + self.xml_page_url, callback=self.parse_xml)
+    # for next_page in response.css('a.next-posts-link'):
+    #     yield response.follow(next_page, self.parse)
+
+  def parse_xml(self, response):
+    f = open("data.txt","w+")
+    products = response.css('tr')
+    # ignore the table header row
+    for product in products[1:]:
+       item = dict()
+       item['seq'] = product.xpath('td[1]//text()').extract_first()
+       item['description'] = product.xpath('td[2]//text()').extract_first()
+       item['document'] = product.xpath('td[3]//text()').extract_first()
+       item['type'] = product.xpath('td[3]//text()').extract_first()
+       item['size'] = product.xpath('td[3]//text()').extract_first()
+       for k, v in item.items():
+        if v is None:
+          item[k] = ""
+       val_string = ','.join(item.values())
+       f.write(val_string + '\n')
