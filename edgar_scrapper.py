486508561c1fe3c7b1383691ca169b531aa0d2a2
Wilfred Ruck <wilfredoruck@gmail.com>
2019-07-21 13:44:13 -0400

    clean up code

--- edgar_scrapper.py
+++ edgar_scrapper.py
@@ -1,12 +1,10 @@
 import scrapy
-from bs4 import BeautifulSoup
 
-class BlogSpider(scrapy.Spider):
-  name = 'blogspider'
+class EdgarSpider(scrapy.Spider):
+  name = 'edgarspider'
   cik = '0001166559'
   base_url = 'https://www.sec.gov'
   xml_page_url = ''
-  # start_url = 'https://www.sec.gov/Archives/edgar/data/1166559/000110465919029714/0001104659-19-029714-index.htm'
   start_url = 'https://www.sec.gov/edgar/searchedgar/companysearch.html'
   start_urls = [start_url]
 
@@ -18,17 +16,6 @@ class BlogSpider(scrapy.Spider):
         callback=self.parse_filings
     )
 
-  # def parse_filings(self, response):
-  #   filings = []
-  #   for filing in response.css('div#contentDiv'):
-  #     filings.append(filing.css('div.noCompanyMatch::text').get())
-  #     if file is not None and "13F" in file:
-  #       filings.append(filing.css('td::text').get())
-  #   for next_page in response.css('a.next-posts-link'):
-  #       yield response.follow(next_page, self.parse_filings)
-  #   print(filings)
-  #   print(response.url)
-
   def parse_filings(self, response):
     for filing in response.css('tr'):
       file = filing.css('td::text').get()
@@ -36,22 +23,26 @@ class BlogSpider(scrapy.Spider):
         self.xml_page_url = filing.css('a#documentsbutton::attr(href)').extract()[0]
         break
     yield scrapy.Request(self.base_url + self.xml_page_url, callback=self.parse_xml)
-    # for next_page in response.css('a.next-posts-link'):
-    #     yield response.follow(next_page, self.parse)
 
   def parse_xml(self, response):
     f = open("data.txt","w+")
     products = response.css('tr')
-    # ignore the table header row
-    for product in products[1:]:
+    for i, product in enumerate(products):
        item = dict()
-       item['seq'] = product.xpath('td[1]//text()').extract_first()
-       item['description'] = product.xpath('td[2]//text()').extract_first()
-       item['document'] = product.xpath('td[3]//text()').extract_first()
-       item['type'] = product.xpath('td[3]//text()').extract_first()
-       item['size'] = product.xpath('td[3]//text()').extract_first()
-       for k, v in item.items():
-        if v is None:
-          item[k] = ""
+       if i == 0:
+        item['seq'] = product.xpath('th[1]//text()').extract_first()
+        item['description'] = product.xpath('th[2]//text()').extract_first()
+        item['document'] = product.xpath('th[3]//text()').extract_first()
+        item['type'] = product.xpath('th[4]//text()').extract_first()
+        item['size'] = product.xpath('th[5]//text()').extract_first()
+       else:
+        item['seq'] = product.xpath('td[1]//text()').extract_first()
+        item['description'] = product.xpath('td[2]//text()').extract_first()
+        item['document'] = product.xpath('td[3]//text()').extract_first()
+        item['type'] = product.xpath('td[4]//text()').extract_first()
+        item['size'] = product.xpath('td[5]//text()').extract_first()
+        for k, v in item.items():
+          if v is None:
+            item[k] = ""
        val_string = ','.join(item.values())
        f.write(val_string + '\n')
